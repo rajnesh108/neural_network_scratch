3a89fab3fdf00a3fc53a9e3fbe369549aacab9c0
Rajnesh Thakur <rajnesh.developer@outlook.com>
2026-01-31 17:42:51 +0530

    add 1

--- basic_files/p004-Layers-and-Object.py
+++ basic_files/p004-Layers-and-Object.py
@@ -14,14 +14,16 @@ X = [[1, 2, 3, 2.5],
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        print("weights", self.weights)
         self.biases = np.zeros((1, n_neurons))
+        print("biases", self.biases)
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
 layer1 = Layer_Dense(4,5)
 layer2 = Layer_Dense(5,2)
-
+ 
 layer1.forward(X)
-#print(layer1.output)
+print(layer1.output) 
 layer2.forward(layer1.output)
 print(layer2.output)
